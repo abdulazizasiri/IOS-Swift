a22dc3dc1f89d147715da365ab8d793d6ac2a422
abdulazizasiri <aasiri@lion.lmu.edu>
2018-05-16 05:47:50 -0700

    Create control_flow.swift
    use of if statements in swift.

---
+++ control_flow.swift
@@ -0,0 +1,14 @@
+import Foundation
+// IF statments:
+
+var x = 5;
+var y = 6;
+
+if (x > y) {
+    print("x is greater than y")
+} else if (x < y) {
+    print("x is less than y")
+} else {
+    print("Equals")
+}
+

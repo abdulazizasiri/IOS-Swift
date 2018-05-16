b223c795be9a628699b17332732658f26e895b44
abdulazizasiri <aasiri@lion.lmu.edu>
2018-05-16 07:20:24 -0700

    Update chaining_returned_value.swift

--- chaining_returned_value.swift
+++ chaining_returned_value.swift
@@ -1,1 +1,17 @@
 
+// functions in swift start with func
+//: # Strings
+
+import Foundation
+
+// Chaining functions
+
+func addExcitementToString(string: String) -> String {
+    return string + "!"
+}
+
+let excitedString = addExcitementToString(string: addExcitementToString(string: "yay"))
+
+print(excitedString)
+let reallyExcitedString = addExcitementToString(string: addExcitementToString(string: addExcitementToString(string: addExcitementToString(string: "wahoo"))))
+print(reallyExcitedString)

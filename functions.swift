be180fd55d1e41768e43adb8549db80fe8dbca03
abdulazizasiri <aasiri@lion.lmu.edu>
2018-05-16 06:53:52 -0700

    function declaration in swift
    Note: 
    
    -> Type is the return value from this function
    
    number: Int - label followed by the data type. 
    
    The return statement is the same as in other programming languages.

--- functions.swift
+++ functions.swift
@@ -1,1 +1,18 @@
 // functions in swift start with func
+//: # Strings
+
+import Foundation
+
+func average (item: [Int]) -> Int {
+    var sum = 0 ;
+    for i in item {
+        sum+=i
+    }
+    return sum / item.count
+}
+
+
+var items = [1,2,3,4,5];
+var result = average(item: items)
+
+print(result);

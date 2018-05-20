ac1d458d1920fad8ee04b44e65da74838f33c16b
abdulazizasiri <aasiri@lion.lmu.edu>
2018-05-20 01:09:06 -0700

    Create function_with_dict.swift
    An example that demonstrates filtering a dictionary of movies with year:name

---
+++ function_with_dict.swift
@@ -0,0 +1,24 @@
+class MovieArchive {
+    func filterByYear(year: Int, movies: Dictionary<String, Int>)  -> [String]{
+        var filteredArray = [String]()
+        for (movie, releaseDate) in movies {
+            if year == releaseDate {
+                filteredArray.append(movie)
+            }
+        }
+        return filteredArray
+        
+    }
+    
+}
+
+var aiThemedMovies = ["Metropolis": 1927, "2001: A Space Odyssey": 1968, "Blade Runner": 1982, "War Games": 1983, "Terminator": 1984, "The Matrix": 1999, "A.I.": 2001, "Her": 2013, "Ex Machina": 2015]
+
+
+var movies = MovieArchive();
+var filteredArray = [String]()
+filteredArray = movies.filterByYear(year: 1999, movies: aiThemedMovies)
+
+for i in filteredArray{
+    print(i)
+}

ccd0cacaabab7db206fca4a7cba37550cc3010a2
abdulazizasiri <aasiri@lion.lmu.edu>
2018-10-30 14:37:44 -0400

    sleepy swift

---
+++ optionals.swift
@@ -0,0 +1,7 @@
+/*
+
+ou use optionals in situations
+ where a value may be absent. An optional represents two possibilities:
+  Either there is a value, and you can unwrap the optional to access that value, or there isn’t a value at all.
+
+*/

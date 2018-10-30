51aaa5d5e008c4221e85f0e10ae5ed186400f499
abdulazizasiri <aasiri@lion.lmu.edu>
2018-10-30 13:40:43 -0400

    added some code for swift

---
+++ Const_Var.swift
@@ -0,0 +1,15 @@
+// Constance vs variables.
+
+// let vs var.
+
+// let is immutbale whereas vasr is mutable.
+
+let maximumNumberOfLoginAttempts = 10
+var currentLoginAttempt = 0
+
+
+// ERROR
+maximumNumberOfLoginAttempts = 1
+
+print(maximumNumberOfLoginAttempts)
+

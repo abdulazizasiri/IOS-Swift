2c06e87c6d1b9220ef0d5390fcfd78745af3c5ee
Abdulaziz Asiri <asiriaabdulaziz@gmail.com>
2018-11-04 18:55:56 -0500

    Optionals

---
+++ intro/optionals.swift
@@ -0,0 +1,8 @@
+// Optionals are a ways to give a var/let two values. But, We do not know which one will be.
+
+// 1- If it happens to be the actual value then You must unwrap it in order to use it.
+
+// 2- It becomes nill. No value found. 
+
+// Examples
+

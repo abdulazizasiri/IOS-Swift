b33e61e6b051a2685c9a93f405d81500fb53f29d
Abdulaziz Asiri <asiriaabdulaziz@gmail.com>
2018-11-05 11:28:41 -0500

    Arrays
    it is an easy type of collection. :)

---
+++ intro/array.swift
@@ -0,0 +1,14 @@
+// Arrays
+var someInts = [Int]()
+someInts.append(3)
+someInts.append(1)
+someInts.append(5)
+
+//print("someInts is of type [Int] with \(someInts.count) items. the vALUE IS \(someInts)")
+
+
+let x = [32,3,35]
+print(type(of: x))
+print(nums)
+var y = x + someInts
+

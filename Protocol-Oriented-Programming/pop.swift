abc4da0b188b8915b2e39413d49f868885d0a87d
Abdulaziz Asiri <asiriaabdulaziz@gmail.com>
2018-11-08 17:11:16 -0500

    swift protocols

--- Protocol-Oriented-Programming/pop.swift
+++ Protocol-Oriented-Programming/pop.swift
@@ -1,1 +1,47 @@
+// protocols in general
 
+protocol CryptoCurrency {
+    var name:String {get}
+    var price: Int {get set}
+    func showHistroy() -> Void
+    mutating func changeBit()
+    init(name:String)
+    
+}
+
+struct Bitcoin:CryptoCurrency {
+    var name: String
+    
+    var price: Int
+    func showHistroy() {
+        print("Bitcoin history")
+    }
+    
+   mutating func changeBit(){
+        print("We are changing bits")
+    }
+    
+      init(name:String){
+        self.name = name
+    }
+}
+class Ethereum:CryptoCurrency {
+    
+    var name: String
+    
+    var price: Int
+    init(name: String, price: Int){
+      self.name = name
+      self.price = price
+    }
+    func showHistroy() {
+        print("Eth history")
+    }
+    
+    func changeBit(){
+        print("We are changing bits")
+    }
+   required init(name:String){
+        self.name = name
+    }
+}

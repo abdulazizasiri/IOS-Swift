22805f28a7d38f8e7cca6f91164097d6f52d446c
Abdulaziz Asiri <asiriaabdulaziz@gmail.com>
2018-11-09 13:20:23 -0500

    guard let vs if let

---
+++ intro/guard.swift
@@ -0,0 +1,40 @@
+class Person {
+    var name:String?
+    var age:Int?
+    
+    init(name:String?, age:Int?) {
+        self.name = name
+        self.age = age
+    }
+    
+}
+func name() {
+var person = Person(name: nil, age: 123)
+
+guard let personName = person.name else {
+    print("Name is not defined")
+    return
+}
+    print("The name is \(personName)")
+    
+    // Better than
+    
+    if let personName2 = person.name {
+        print(personName2)
+    } else {
+        print("Name is not there")
+    }
+    
+
+
+
+}
+
+name()
+
+// Two benefits of using guard let vs if let for optional unwrapping. 
+
+// 1- If you have a class with many optionals, then you want to to do optional wrapping, then we will end up with many nesting blocks.
+// the run time can lead to O(n*n)
+
+// 2- The scope of guard to use without force unwrapping is the whole function. The scope of if let is ONLY the block where you define the the block of if let

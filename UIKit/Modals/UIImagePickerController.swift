58b86169cf0f5f9a0f31a174c4d84aa02669a1b1
Abdulaziz Asiri <asiriaabdulaziz@gmail.com>
2018-11-15 01:33:10 -0500

    Update UIImagePickerController.swift

--- UIKit/Modals/UIImagePickerController.swift
+++ UIKit/Modals/UIImagePickerController.swift
@@ -1,1 +1,7 @@
 
+    @IBAction func presentView(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+        present(imagePicker, animated: true,completion: nil)
+        
+        
+    }

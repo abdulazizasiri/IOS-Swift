d8c6d3ca387b70b7e683045557c91ca132d043d2
Abdulaziz Asiri <asiriaabdulaziz@gmail.com>
2018-11-15 01:31:42 -0500

    Example of an alert modal

--- UIKit/Modals/UiAlertController.swift
+++ UIKit/Modals/UiAlertController.swift
@@ -1,1 +1,16 @@
 
+      @IBAction func add(_ sender: Any) {
+        let alert = UIAlertController()
+        
+        alert.title = "Add Location"
+        alert.message = "No thank you"
+     
+
+        let ocAction = UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: {
+            action in self.dismiss(animated:true, completion:nil)
+        })
+        alert.addAction(ocAction)
+        present(alert, animated: true,completion: nil)
+   
+        
+    }

8a8d9705b7adf7965d0b6b32eec62089bd1daf68
Abdulaziz Asiri <asiriaabdulaziz@gmail.com>
2018-11-20 05:10:18 -0500

    model

---
+++ MVC/Model/Concentration.swift
@@ -0,0 +1,14 @@
+//
+//  Concentration.swift
+//  MatchMatch
+//
+//  Created by Abdulaziz Asiri on 11/20/18.
+//  Copyright © 2018 Abdulaziz Asiri. All rights reserved.
+//
+
+import Foundation
+// Note: In the model, we do not import UIKit in the project. 
+
+class Concentration {
+    
+}

687bf8de627380864e3204d2590436ceab52e9e3
Abdulaziz Asiri <aasiri@lion.lmu.edu>
2018-12-30 02:08:21 +0300

    Create dataTypes.swift

---
+++ Swift/dataTypes.swift
@@ -0,0 +1,12 @@
+//
+// Example of datatyoe: String, Int, Float, Double, Bool
+
+// Explictily define the type.
+var x:Int
+
+
+// implicitly sets the type to int (because of inference)
+var xy = 12
+
+
+// Note: You cannot change the type after giving it an initial value of some type. 

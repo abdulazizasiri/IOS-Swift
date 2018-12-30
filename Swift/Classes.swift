da3eac1bd4d83e54cd00e1011e1a5a4dce9e3448
Abdulaziz Asiri <aasiri@lion.lmu.edu>
2018-12-30 17:31:30 +0300

    Update Classes.swift

--- Swift/Classes.swift
+++ Swift/Classes.swift
@@ -19,3 +19,28 @@ abdulPost.title = "Swift"
 
 print(azizPost===abdulPost)
 
+
+
+// Inheritance 
+
+
+class Cars {
+    var topSpeed = 200
+    var type = "Normal"
+    
+    func drive () {
+        print("The type of the car is \(type) and its top speed is \(topSpeed)")
+    }
+    
+}
+
+class Toyota: Cars {
+    var fly = true
+    override func drive(){
+        print("I am not any more a normal car")
+    }
+    
+}
+
+Toyota().drive()
+

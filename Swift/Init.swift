16e126a62f3d018ae937797630bc117d8aec9956
Abdulaziz Asiri <aasiri@lion.lmu.edu>
2018-12-30 19:21:14 +0300

    Create Init.swift

---
+++ Swift/Init.swift
@@ -0,0 +1,47 @@
+// UIKIT: A set of classes that are respoinsble to make ios app user interface
+// IT is responsible for building the view. √
+
+
+// initilizers in swift is a big topic:
+
+
+/*
+ 
+ Class inheritance and Initialization
+ Failable initialization
+ Required initialization
+ 
+ */
+
+
+//  Note: The biggest role in initilizrs is the idea that setting initial values for instance variables (of course if they are not set to a default value)
+
+
+// Mutiple initilizers in the same class. They differ in the number of parameters
+
+class Person {
+    var name: String?
+    var age: Int?
+    init(name: String, age:Int) {
+        self.name = name
+        self.age = age
+    }
+    init() {
+        self.name = ""
+        self.age = 0
+    }
+}
+
+var person = Person(name: "Aziz", age:28)
+person.name
+person.age
+
+var person2 = Person(name: "Abdul", age:25)
+
+
+person2.age
+
+
+var p = Person()
+p.name
+p.age

0a3e7e3f1e5eb5863d601e600e8fd20d67cfecae
Abdulaziz Asiri <aasiri@lion.lmu.edu>
2018-12-31 19:03:19 +0300

    Create Properties.swift

---
+++ Swift/Properties.swift
@@ -0,0 +1,14 @@
+// Properties :
+
+// TYpes of properties in swift.
+
+//Stored properties
+//Computed properties.
+
+//
+//Stored properties can be either variable stored properties (introduced by the var keyword) or constant stored properties (introduced by the let keyword).
+
+
+// Can be given an initial value OR initilized during initilization .
+
+

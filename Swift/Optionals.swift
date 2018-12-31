9309f969bdb5eb71fa94985300dc21e3e710ab99
Abdulaziz Asiri <aasiri@lion.lmu.edu>
2018-12-31 17:54:22 +0300

    Create Optionals.swift

---
+++ Swift/Optionals.swift
@@ -0,0 +1,29 @@
+
+// This file does not include any code but, it only has comments about how to use optionals in code. 
+
+// Optionals.
+
+// IT is a way to tell the compiler currently , I do not know.
+
+
+// it uses ? to tell the compiler this piece of info.
+
+// Note you have to  to do the folllwing to check if the value still optional or not,
+
+//1- If Statements and Forced Unwrapping
+
+
+//if convertedNumber != nil {
+//    print("convertedNumber has an integer value of \(convertedNumber!).")
+//}
+
+
+// 2- Optional Binding
+
+//if let actualNumber = Int(possibleNumber) {
+//    print("The string \"\(possibleNumber)\" has an integer value of \(actualNumber)")
+//} else {
+//    print("The string \"\(possibleNumber)\" could not be converted to an integer")
+//}
+
+

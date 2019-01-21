1d3573792a7626996723541da2bc0f81af667ba6
Abdulaziz Asiri <asiriaabdulaziz@gmail.com>
2019-01-21 17:15:54 +0300

    Update Closures.swift

--- Swift/Closures.swift
+++ Swift/Closures.swift
@@ -1,1 +1,8 @@
-// Not yet
+// Closures + Functional prgoamming + Function types + higher order functions + nested function
+
+/*
+ Closures are self-contained blocks of functionality that can 
+be passed around and used in your code. Closures in Swift are similar to blocks in C and Objective-C and to lambdas in other programming languages.
+ */
+
+

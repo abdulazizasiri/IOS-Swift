b293de4635d3403bab7bcddb4562f2e5ee304473
Abdulaziz Asiri <asiriaabdulaziz@gmail.com>
2019-01-28 00:14:57 +0300

    Update Classes_Struct.swift

--- Swift/Classes_Struct.swift
+++ Swift/Classes_Struct.swift
@@ -1,1 +1,31 @@
 
+// apple example:
+
+// Creating a structure using the strcuct keyword. 
+struct Resolution {
+    var width:Int
+    var height: Int
+}
+class VideoMode {
+
+    var interlaced:Bool
+    var frameRate: Int
+    var name: String
+    
+//    If not default intimidation given to the props, then initialize them inside an init
+    init(){
+        self.frameRate = 0
+        self.interlaced = false
+        self.name = ""
+    }
+}
+
+
+// Creating an instance
+let someResolution = Resolution(width: 1, height: 1)
+let someVideoMode = VideoMode()
+
+
+// To access the properties of a class or a strcuture, we use the dot operator.
+
+

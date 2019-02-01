e5aadf21d4e75538b13e0acfb49bf0f20bb7222d
Abdulaziz Asiri <asiriaabdulaziz@gmail.com>
2019-02-01 03:39:36 +0300

    Update Properties.swift
    Stored props

--- Swift/Properties.swift
+++ Swift/Properties.swift
@@ -12,3 +12,39 @@
 // Can be given an initial value OR initilized during initilization .
 
 
+// propoerties .
+
+
+// There are three areas:
+//
+//1- Stored properties
+//2- Computed  properties
+//3- Observers
+
+
+// Examples: This is example of stored properties
+
+// This is an example of a calss that uses a dot notation to access its props:
+
+class Person {
+ // We intilized them with "IDK"
+    var name:String?
+    var age:Int?
+}
+
+
+class Blog {
+    var title:String?
+    var body = "hey"
+    var author:Person?
+    var comments = 0
+}
+
+let myBlog = Blog() // Creating an instances.
+
+myBlog.author!.name = "Aziz"
+myBlog.title = "This a blog about Sswift programming language"
+
+
+
+
